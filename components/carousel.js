9821c118f297acfa6428a8108a46103270bbda4b
James Ofori Ayerakwa <jamesofoaye@gmail.com>
2021-01-02 21:47:29 +0000

    Update

--- components/carousel.js
+++ components/carousel.js
@@ -1,7 +1,7 @@
 import { Carousel } from 'react-bootstrap';
 export default function carousel() {
     return (
-        <Carousel className="pt-12">
+        <Carousel className="pt-6">
             <Carousel.Item>
                 <img
                     className="d-block w-screen h-1/2"
