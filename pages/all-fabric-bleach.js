5d20eee06cebd7e7760103ad2b6329cd4f5ca74f
James Ofori Ayerakwa <jamesofoaye@gmail.com>
2021-01-03 17:03:59 +0000

    Update

--- pages/all-fabric-bleach.js
+++ pages/all-fabric-bleach.js
@@ -2,12 +2,21 @@ import Head from 'next/head'
 import Navbar from '../components/nav'
 import Footer from '../components/footer'
 import React from "react";
+
+const details = {
+        title: "Amway Home™ All Fabric Bleach",
+        id: "Amway Home All Fabric Bleach",
+        price: "22.30",
+        image: "/images/all-fabric-bleach.jpg"
+}
+
 export default function All_Fabric_Bleach() {
     return (
         <div className="">
             <Head>
                 <title>Embill Store - Save Time, Save Money, Everyday!!!</title>
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.27/default/snipcart.css" />
             </Head>
 
             <main>
@@ -42,6 +51,26 @@ export default function All_Fabric_Bleach() {
                                         Do not use on wool or silk, or with chlorine bleach.
 
 To presoak: Dissolve one scoop (30 mL) per load and soak 30 minutes or longer, then wash as normal.</p>
+                                        <div className="flex space-x-3 mb-4 text-sm font-medium mt-4">
+                                            <div className="flex-auto flex space-x-4">
+                                                <button className="snipcart-add-item snipcart-checkout w-1/2 h-9 flex items-center justify-center rounded-md bg-black text-white" type="submit"
+                                                        data-item-id={details.id}
+                                                        data-item-price={details.price}
+                                                        data-item-url="/"
+                                                        data-item-image={details.image}
+                                                        data-item-name={details.title}>
+                                                    Buy now
+                                                </button>
+                                                <button className="snipcart-add-item w-1/2  h-9 flex items-center justify-center rounded-md border border-gray-300" type="button"
+                                                        data-item-id={details.id}
+                                                        data-item-price={details.price}
+                                                        data-item-url="/"
+                                                        data-item-image={details.image}
+                                                        data-item-name={details.title}>
+                                                    Add to bag
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -50,6 +79,14 @@ To presoak: Dissolve one scoop (30 mL) per load and soak 30 minutes or longer, t
                 </div>
                 <Footer />
             </main>
+            <script async src="https://cdn.snipcart.com/themes/v3.0.27/default/snipcart.js"></script>
+            <div
+                id="snipcart"
+                data-config-add-product-behavior="none"
+                data-config-modal-style="side"
+                data-api-key="NGNhY2MzOTEtYzdiMS00ODk0LWI5NzMtNTgyMjFiZGFlZmVhNjM3MzYwNjg4OTAxNTYxMDAy"
+                hidden>
+            </div>
         </div>
     )
 }
